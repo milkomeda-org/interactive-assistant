13479d1624b3132a56cb5c33ed33ecfd0c6fda27
vinson <wincentlauvinson@gmail.com>
2019-05-27 11:04:26 +0800

    全部使用Kotlin

---
+++ src/main/java/com/lauvinson/open/translation/utils/JsonUtils.kt
@@ -0,0 +1,39 @@
+package com.lauvinson.open.translation.utils
+
+object JsonUtils {
+
+    private fun getLevelStr(level: Int): String {
+        val levelStr = StringBuilder()
+        for (levelI in 0 until level) {
+            levelStr.append("\t")
+        }
+        return levelStr.toString()
+    }
+
+    fun JsonFormart(s: String): String {
+        var level = 0
+        //存放格式化的json字符串
+        val jsonForMatStr = StringBuilder()
+        for (index in 0 until s.length) {
+            val c = s[index]
+            if (level > 0 && '\n' == jsonForMatStr[jsonForMatStr.length - 1]) {
+                jsonForMatStr.append(getLevelStr(level))
+            }
+            when (c) {
+                '{', '[' -> {
+                    jsonForMatStr.append(c).append("\n")
+                    level++
+                }
+                ',' -> jsonForMatStr.append(c).append("\n")
+                '}', ']' -> {
+                    jsonForMatStr.append("\n")
+                    level--
+                    jsonForMatStr.append(getLevelStr(level))
+                    jsonForMatStr.append(c)
+                }
+                else -> jsonForMatStr.append(c)
+            }
+        }
+        return jsonForMatStr.toString()
+    }
+}
