6ec09165038cb9f3fc13c0d464a8ad38dae9f829
vinson <wincentlauvinson@gmail.com>
2019-05-27 11:03:50 +0800

    使用Kotlin

---
+++ src/main/java/com/lauvinson/open/translation/TranslateAction.kt
@@ -0,0 +1,47 @@
+package com.lauvinson.open.translation
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.SelectionModel
+import com.intellij.openapi.ui.popup.Balloon
+import com.intellij.openapi.ui.popup.JBPopupFactory
+import com.intellij.ui.JBColor
+import com.lauvinson.open.translation.utils.HttpUtils
+import com.lauvinson.open.translation.utils.JsonUtils
+import org.apache.http.util.TextUtils
+
+import java.awt.*
+import java.nio.charset.StandardCharsets
+import java.util.HashMap
+
+/**
+ * @author created by vinson on 2019/5/22
+ */
+class TranslateAction : AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        // TODO: insert action logic here
+        val mEditor = e.getData(PlatformDataKeys.EDITOR) ?: return
+        val model = mEditor.selectionModel
+        val selectedText = model.selectedText
+        if (TextUtils.isEmpty(selectedText)) {
+            return
+        }
+        val response = JsonUtils.JsonFormart(HttpUtils.URLGet("http://fanyi.youdao.com/openapi.do?keyfrom=neverland&key=969918857&type=data&doctype=json&version=1.1&q=" + selectedText!!, HashMap(), StandardCharsets.UTF_8.displayName())!!)
+        println(response)
+        showPopupBalloon(mEditor, response)
+    }
+
+    private fun showPopupBalloon(editor: Editor, result: String) {
+        ApplicationManager.getApplication().invokeLater {
+            val factory = JBPopupFactory.getInstance()
+            factory.createHtmlTextBalloonBuilder(result, null, JBColor(Color(186, 238, 186), Color(73, 117, 73)), null)
+                    .setFadeoutTime(5000)
+                    .createBalloon()
+                    .show(factory.guessBestPopupLocation(editor), Balloon.Position.below)
+        }
+    }
+}
