100e0ded3a7477ecdce48f4ef196c45c87eae1db
vinson <liuws@zhiyun-tech.com>
2020-12-10 17:36:46 +0800

    feat(setting): disable up&down action

--- src/main/java/com/lauvinson/source/open/assistant/ui/ToolWindowFactory.java
+++ src/main/java/com/lauvinson/source/open/assistant/ui/ToolWindowFactory.java
@@ -21,11 +21,15 @@ package com.lauvinson.source.open.assistant.ui;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.ui.JBColor;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.util.concurrent.TimeUnit;
 
 public class ToolWindowFactory implements com.intellij.openapi.wm.ToolWindowFactory, DumbAware {
 
@@ -57,7 +61,8 @@ public class ToolWindowFactory implements com.intellij.openapi.wm.ToolWindowFact
         }
 
         JPanel createToolWindowPanel() {
-            return panel;
+            JPanel star = new draw_star();
+            return star;
         }
 
         private void resetStats() {
@@ -68,4 +73,81 @@ public class ToolWindowFactory implements com.intellij.openapi.wm.ToolWindowFact
 
         }
     }
+
+    static class thread_star extends Thread {
+        int x0;
+        int y0;
+        int r0;
+        int d0;
+        double angle;
+        thread_star(int x,int y,int r,double a)
+        {
+            x0=x;
+            y0=y;
+            r0=r;
+            d0=x0-760;
+            angle=a;
+        }
+        public void run() {
+            double an=angle/3;
+            while(true)
+            {
+                x0=(int) (760+d0*Math.cos(angle));
+                y0=(int) (440+d0*Math.sin(angle));
+                angle=angle+an/10;
+                try {
+                    TimeUnit.MILLISECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+        }
+    }
+
+    static class draw_star extends JPanel {
+
+        thread_star[] s = {new thread_star(1200, 440, 30, (Math.PI / 20)), new thread_star(1600, 440, 40, (Math.PI / 40))
+                , new thread_star(1300, 440, 25, (Math.PI / 30)), new thread_star(1520, 440, 30, (Math.PI / 36)),
+                new thread_star(1080, 440, 18, (Math.PI / 10))
+        };
+
+        draw_star() {
+            for (ToolWindowFactory.thread_star thread_star : s) thread_star.start();
+        }
+
+        public void paint(Graphics g) {
+            g.fillRect(0, 0, getWidth(), getHeight());
+            Image offScreenImage = this.createImage(this.getWidth(), this.getHeight());
+            Graphics gImage = offScreenImage.getGraphics();
+            super.paint(gImage);
+            g.drawImage(offScreenImage, 0, 0, null);
+        }
+
+        public void paintComponent(Graphics g0) {
+            Graphics2D g;
+            Color[] c = {
+                    JBColor.RED,
+                    JBColor.BLUE,
+                    JBColor.WHITE,
+                    JBColor.BLACK,
+                    JBColor.GRAY,
+                    JBColor.LIGHT_GRAY,
+                    JBColor.DARK_GRAY,
+                    JBColor.PINK,
+                    JBColor.ORANGE,
+                    JBColor.YELLOW,
+                    JBColor.GREEN,
+                    JBColor.MAGENTA,
+                    JBColor.CYAN,
+            };
+            for (int i = 0; i < s.length; i++) {
+                g = (Graphics2D) (g0);
+                g.setColor(c[i]);
+                Ellipse2D ellipse2D = new Ellipse2D.Double(s[i].x0 - s[i].r0, s[i].y0 - s[i].r0, 2 * s[i].r0, 2 * s[i].r0);
+                g.fill(ellipse2D);
+            }
+            repaint();
+        }
+    }
+
 }
